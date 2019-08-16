a3444a9bd7c12d0a3ca290e1cec5b0d5d1395faa
Bytekeeper <therealdante@gmail.com>
2019-08-16 15:04:33 +0200

    Added GMS helper class for managing resources.

---
+++ src/main/java/org/bk/ass/GMS.java
@@ -0,0 +1,67 @@
+package org.bk.ass;
+
+import bwapi.TechType;
+import bwapi.UnitType;
+import bwapi.UpgradeType;
+
+import static java.lang.Integer.max;
+
+/** Representing gas + minerals + supply */
+public final class GMS {
+  final int gas;
+  final int minerals;
+  final int supply;
+
+  public GMS(int gas, int minerals, int supply) {
+    this.gas = gas;
+    this.minerals = minerals;
+    this.supply = supply;
+  }
+
+  public static GMS unitCost(UnitType unitType) {
+    return new GMS(unitType.gasPrice(), unitType.mineralPrice(), unitType.supplyRequired());
+  }
+
+  public static GMS unitCost(org.openbw.bwapi4j.type.UnitType unitType) {
+    return new GMS(unitType.gasPrice(), unitType.mineralPrice(), unitType.supplyRequired());
+  }
+
+  public static GMS techCost(TechType techType) {
+    return new GMS(techType.gasPrice(), techType.mineralPrice(), 0);
+  }
+
+  public static GMS techCost(org.openbw.bwapi4j.type.TechType techType) {
+    return new GMS(techType.gasPrice(), techType.mineralPrice(), 0);
+  }
+
+  public static GMS researchCost(UpgradeType upgradeType, int level) {
+    return new GMS(upgradeType.gasPrice(level), upgradeType.mineralPrice(level), 0);
+  }
+
+  public static GMS researchCost(org.openbw.bwapi4j.type.UpgradeType upgradeType, int level) {
+    return new GMS(upgradeType.gasPrice(level), upgradeType.mineralPrice(level), 0);
+  }
+
+  public GMS subtract(GMS gms) {
+    return new GMS(gas - gms.gas, minerals - gms.minerals, supply - gms.supply);
+  }
+
+  public GMS add(GMS gms) {
+    return new GMS(gas + gms.gas, minerals + gms.minerals, supply + gms.supply);
+  }
+
+  /**
+   * If all components (gas, minerals, supply) of this value are non-negative, returns true if all
+   * are &gt;= that of the given value. If a component is negative, returns false if the given
+   * value's component is &gt;= 0. <br>
+   * Usually all components are &gt;= 0 and this can be used to check if a price could be payed
+   * <em>now</em>. <br>
+   * If planning ahead is used, components could already be negative. But if the cost (ie. supply)
+   * is 0, it might still be possible to purchase it immediately.
+   */
+  public boolean greaterOrEqual(GMS gms) {
+    return max(0, gas) >= gms.gas
+        && max(0, minerals) >= gms.minerals
+        && max(0, supply) >= gms.supply;
+  }
+}
