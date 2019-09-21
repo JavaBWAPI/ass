31ce90f2d2ad5c55e5e15457f31b7944bd9672b0
Bytekeeper <therealdante@gmail.com>
2019-09-21 19:05:54 +0200

    Additional frameskip benchmark

--- src/jmh/java/org/bk/ass/SimulatorBenchmark.java
+++ src/jmh/java/org/bk/ass/SimulatorBenchmark.java
@@ -11,7 +11,7 @@ import org.openjdk.jmh.annotations.*;
 public class SimulatorBenchmark {
 
   @State(Scope.Thread)
-  public static class MyState {
+  public static class DefaultSetup {
 
     Simulator simulator;
     BWAPI4JAgentFactory factory = new BWAPI4JAgentFactory(null);
@@ -19,6 +19,25 @@ public class SimulatorBenchmark {
     @Setup(Level.Invocation)
     public void setup() {
       simulator = new Simulator();
+
+      for (int i = 0; i < 30; i++) {
+        simulator.addAgentA(factory.of(UnitType.Zerg_Mutalisk));
+      }
+      for (int i = 0; i < 30; i++) {
+        simulator.addAgentB(factory.of(UnitType.Zerg_Hydralisk));
+      }
+    }
+  }
+
+  @State(Scope.Thread)
+  public static class WithFrameSkipFour {
+
+    Simulator simulator;
+    BWAPI4JAgentFactory factory = new BWAPI4JAgentFactory(null);
+
+    @Setup(Level.Invocation)
+    public void setup() {
+      simulator = new Simulator(4);
 
       for (int i = 0; i < 30; i++) {
         simulator.addAgentA(factory.of(UnitType.Zerg_Mutalisk));
@@ -38,12 +57,17 @@ public class SimulatorBenchmark {
   }
 
   @Benchmark
-  public int _30MutasVs30Hydras(MyState state) {
+  public int _30MutasVs30Hydras(DefaultSetup state) {
     return state.simulator.simulate(-1);
   }
 
   @Benchmark
-  public int clearCollisionMaps(MyState state) {
+  public int _30MutasVs30Hydras_fs4(WithFrameSkipFour state) {
+    return state.simulator.simulate(-1);
+  }
+
+  @Benchmark
+  public int clearCollisionMaps(DefaultSetup state) {
     state.simulator.reset();
     return state.simulator.getAgentsA().size() + state.simulator.getAgentsB().size();
   }
